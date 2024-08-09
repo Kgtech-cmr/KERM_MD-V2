e6c0f75d7dad83cf55649b1fb4595f2ca80efe0b
KermRayan. <kermd237@gmail.com>
2024-08-09 16:16:27 +0100

    KERM V2

--- plugins/search.js
+++ plugins/search.js
@@ -5,24 +5,50 @@ const axios = require('axios')
 const fetch = require('node-fetch')
 smd(
   {
-    const {
-    Module
-} = require('../main');
-const { getJson } = require("./misc/misc");
-const {MODE} = require('../config');
-let fromMe = MODE == 'public' ? false : true
-Module({
-    pattern: 'lyrics ?(.*)',
-    fromMe: fromMe,
-    desc: 'Lyrics search & find',
-    use: 'download'
-}, (async (message, match) => {
-    if (!match[1]) return await message.sendReply("_Need query!_");
-    let result = await getJson(`https://raganork.tk/api/lyrics?query=${encodeURIComponent(match[1])}`)
-    if (!result.status) return await message.sendReply('_No results found!_')
-    let ch = '```'
-    return await message.sendReply(`${ch}Title: "${result.title}"\n\n${result.result}${ch}`);
-}));
+    pattern: "lyrics",
+    desc: "Get the lyrics of a song.",
+    category: "search",
+    filename: __filename,
+    use: "<song_name>",
+  },
+  async (m, songName) => {
+    try {
+      if (!songName) {
+        return await m.send("*_Please provide a song name!_*");
+      }
+
+      const apiUrl = `https://api.maher-zubair.tech/search/lyrics?q=${encodeURIComponent(
+        songName
+      )}`;
+      const response = await fetch(apiUrl);
+
+      if (!response.ok) {
+        return await m.send(
+          `*_Error: ${response.status} ${response.statusText}_*`
+        );
+      }
+
+      const data = await response.json();
+
+      if (data.status !== 200) {
+        return await m.send("*_An error occurred while fetching the data._*");
+      }
+
+      const { artist, lyrics, title } = data.result;
+
+      const lyricsMessage = `
+*Song:* ${title}
+*Artist:* ${artist}
+
+${lyrics}
+`;
+
+      await m.send(lyricsMessage);
+    } catch (e) {
+      await m.error(`${e}\n\ncommand: lyrics`, e);
+    }
+  }
+);
 smd({
     pattern: "bing",
     alias: ["bingsearch"],
@@ -129,7 +155,7 @@ async(message, match) => {
  try{
 
    message.react("🔍")
-         if (!match) return message.reply(`Give me a user name like ${prefix}github KERM_MD-V2`)
+         if (!match) return message.reply(`Give me a user name like ${prefix}github Astropeda`)
 
          const { data } = await axios(`https://api.github.com/users/${match}`)
    if(!data) return await message.send(`*_Didn't get any results, Provide valid user name!_*`)
@@ -245,44 +271,6 @@ smd({
         }catch(e){return await message.error(`${e}\n\n command: weather`,e,`*_Please provide valid city name!_*`) }
         }
     )
- //---------------------------------------------------------------------------
-cmd({
-            pattern: "horo",
-            category: "search",
-            desc: "Gives horoscope info of user.",
-            use: '<sign>\n:Example: horo libra',
-            react: "🔮",
-            filename: __filename,
-        },
-        async(Void, citel, text) => {
-            if (!text) return citel.reply("Provide me a sign bro!")
-            try {
-                const URL = `https://aztro.sameerkumar.website/?sign=${text}&day=today`;
-                fetch(URL, {
-                        method: 'POST'
-                    })
-                    .then(response => response.json())
-                    .then(json => {
-                        const date = json.current_date;
-                        console.log(date);
-                        let textw = "";
-                        textw += `*🌟 Horoscope of  ${text}*\n\n`;
-                        textw += `*Current Date:* ${json.current_date}.\n`;
-                        textw += `*Sign:* ${text}.\n`;
-                        textw += `*Lucky Time:* ${json.lucky_time}.\n`;
-                        textw += `*Compatibility:* ${json.compatibility}.\n`;
-                        textw += `*Lucky Number:* ${json.lucky_number}.\n`;
-                        textw += `*Lucky Color:* ${json.color}.\n`;
-                        textw += `*Today Mood:* ${json.mood}.\n`;
-                        textw += `*Overall:* ${json.description}.\n`;
-                        citel.reply(textw)
-                    });
-
-            } catch (e) {
-                console.log(e)
-            }
-        }
-    )
 //---------------------------------------------------------------------------
 smd({
          pattern: "npm",
@@ -377,7 +365,7 @@ smd({
         },
         async(message, text) => {
           try{
-            if (!text) return message.reply(`*_Uhh please, give me a query_*\n*_Example : ${prefix}google KERM_MD-V2._*`);
+            if (!text) return message.reply(`*_Uhh please, give me a query_*\n*_Example : ${prefix}google RIAS_GREMORY-BOT._*`);
             let google = require('google-it');
             google({ 'query': text}).then(res => {
                 let msg= `Google Search From : ${text} \n\n`;
@@ -529,13 +517,12 @@ smd({
             category: "search",
             desc: "Searches Image on Google",
             use: '<text>',
-            react: "📸",
             filename: __filename,
         },
         async(message, match) => {
 try{
   let text = match ? match : message.reply_text;
-   if (!text) return message.reply(`Provide me a query!\n*Ex : .image Kerm |10*`)
+   if (!text) return message.reply(`Provide me a query!\n*Ex : .image luffy |10*`)
    
    let name1 = text.split("|")[0] || text
    let name2 = text.split("|")[1] || 5
