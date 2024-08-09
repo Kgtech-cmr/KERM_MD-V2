31a3c0dc6016f0c788c3e471eb071db18f5116fe
KermRayan. <kermd237@gmail.com>
2024-08-09 12:49:29 +0100

    Update search.js

--- plugins/search.js
+++ plugins/search.js
@@ -271,6 +271,43 @@ smd({
         }catch(e){return await message.error(`${e}\n\n command: weather`,e,`*_Please provide valid city name!_*`) }
         }
     )
+ //---------------------------------------------------------------------------
+cmd({
+            pattern: "horo",
+            category: "search",
+            desc: "Gives horoscope info of user.",
+            use: '<sign>\n:Example: horo libra',
+            filename: __filename,
+        },
+        async(Void, citel, text) => {
+            if (!text) return citel.reply("Provide me a sign bro!")
+            try {
+                const URL = `https://aztro.sameerkumar.website/?sign=${text}&day=today`;
+                fetch(URL, {
+                        method: 'POST'
+                    })
+                    .then(response => response.json())
+                    .then(json => {
+                        const date = json.current_date;
+                        console.log(date);
+                        let textw = "";
+                        textw += `*🌟 Horoscope of  ${text}*\n\n`;
+                        textw += `*Current Date:* ${json.current_date}.\n`;
+                        textw += `*Sign:* ${text}.\n`;
+                        textw += `*Lucky Time:* ${json.lucky_time}.\n`;
+                        textw += `*Compatibility:* ${json.compatibility}.\n`;
+                        textw += `*Lucky Number:* ${json.lucky_number}.\n`;
+                        textw += `*Lucky Color:* ${json.color}.\n`;
+                        textw += `*Today Mood:* ${json.mood}.\n`;
+                        textw += `*Overall:* ${json.description}.\n`;
+                        citel.reply(textw)
+                    });
+
+            } catch (e) {
+                console.log(e)
+            }
+        }
+    )
 //---------------------------------------------------------------------------
 smd({
          pattern: "npm",
