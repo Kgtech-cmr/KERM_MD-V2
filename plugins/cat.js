d6c7a3af5998a6240b88ef557126ad1b7699239c
KermRayan. <kermd237@gmail.com>
2024-08-11 01:46:05 +0100

    Update cat.js

--- plugins/cat.js
+++ plugins/cat.js
@@ -11,7 +11,7 @@ smd(
     try {
       await m.send(
         "https://cataas.com/cat",
-        { caption: "*meyaoooooooooooooon!*" },
+        { caption: "*Meyaooooooooo🐈!*" },
         "img",
         m
       );
