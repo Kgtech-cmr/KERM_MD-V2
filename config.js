7573faf86045dc82a34fa29024330b0707517249
kamadotanjiro67 <167286888+kamadotanjiro67@users.noreply.github.com>
2024-08-13 14:49:26 +0200

    Update config.js

--- config.js
+++ config.js
@@ -21,8 +21,8 @@ global.devs = "https://wa.me/237650564445 , https://wa.me/237656520674";
 global.sudo = process.env.SUDO || "25766486303";
 global.owner = process.env.OWNER_NUMBER || "25766486303";
 global.style = process.env.STYLE || "3";
-global.gdbye = process.env.GOODBYE || "false";
-global.wlcm = process.env.WELCOME || "false";
+global.gdbye = process.env.GOODBYE || "true";
+global.wlcm = process.env.WELCOME || "true";
 global.warncount = process.env.WARN_COUNT || 3;
 global.disablepm = process.env.DISABLE_PM || "false";
 global.disablegroup = process.env.DISABLE_GROUPS || "false",
@@ -42,7 +42,7 @@ global.scan = "https://kerm-session-39mi.onrender.com/wasiqr";
 
 global.SESSION_ID =
   process.env.SESSION_ID ||
-  ""
+  "eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoia0JCN3E0THZZOW8xN1ZSbmJLdjN0NnJ2NGwxcWk4QnE4SkdxUUFCakoxVT0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoid083OGdjSjZ2a3A0ZFR2Qjl6Ukg2U0t3UTJWSUFhWHAxb3pqV0pONlp6WT0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJNTlZBU3BoM0R0UmRHL1FpdkFIcTJnM1FOVWlSK2l3aWMxdWJwa3lMd0c0PSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJnNmVjeGRUdmxsdHBmSnBEY25vVjlhMTNsanNSdE51TVV0ZFRieGs0Y2hJPSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IjRNd3VOVExqSG1oTm1IOFFyNDNIcFZKVEFKUVU1RVFUZlhIbXF3b1drbHc9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6ImJlOGVUTWp1Q3pGQWhEdnpDWkdXR2dLc2pVbjZnNjlaVTIrUU1sUkJybWc9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiY0FFQ0xQWWNONVlMdFZsVHpGU2hiZG92eXkwRVpZSUNaZEJtQThoM0kyTT0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiaURRQmJBNGMwdktRUCt2dmVFZU5FbXo0Vm1JWW1yamd1MXRjelYvbTBtbz0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IlFXYUxobnh1RHpweHZTNFF3TDBncFlaZm9iNldBeHZKVGJNWnd0Ym8rVzl0QVpYamUwUy8rU3I3bW1iTXpGV1pBNm5sUXI5dlN6MmZwZENjN0tSckRnPT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6MjI0LCJhZHZTZWNyZXRLZXkiOiJqd2tGaG9KaGJFVEpoZ3BkR2t3dkFQR056TnlweVl6WElaQy8zbGNjSjNRPSIsInByb2Nlc3NlZEhpc3RvcnlNZXNzYWdlcyI6W10sIm5leHRQcmVLZXlJZCI6MzEsImZpcnN0VW51cGxvYWRlZFByZUtleUlkIjozMSwiYWNjb3VudFN5bmNDb3VudGVyIjowLCJhY2NvdW50U2V0dGluZ3MiOnsidW5hcmNoaXZlQ2hhdHMiOmZhbHNlfSwiZGV2aWNlSWQiOiJGZHIyR0JmaVFuNmp6VmNINElYLVJ3IiwicGhvbmVJZCI6IjJiMDhhMzJlLTk2MDItNDM3NS05NWUwLTM2YzE2NTIyOGMxNSIsImlkZW50aXR5SWQiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJ3U244UC9ueGJLWGV0M2NZdndKYjYyNCthdkk9In0sInJlZ2lzdGVyZWQiOnRydWUsImJhY2t1cFRva2VuIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiRDQrNjhRYldMc2NrZVg0cTJzMVRlejk2SjNNPSJ9LCJyZWdpc3RyYXRpb24iOnt9LCJwYWlyaW5nQ29kZSI6Ijc1U0Y5QTZHIiwibWUiOnsiaWQiOiIyNTc2NjQ4NjMwMzo3NkBzLndoYXRzYXBwLm5ldCJ9LCJhY2NvdW50Ijp7ImRldGFpbHMiOiJDTHI2a2NNREVMNjY3TFVHR0FFZ0FDZ0EiLCJhY2NvdW50U2lnbmF0dXJlS2V5Ijoia3RrTWpRd1JpZ1A3SUJ5aWFUNlFhQ3FXMlVBdHpPUlduRlhtZjZSRTNHVT0iLCJhY2NvdW50U2lnbmF0dXJlIjoiNUFxOTJQNDdQSG50RTFtajgrOFliK1RvRk1seWFIYm1SaHZWdlk3T2ovMS9qaFI0OGlxdTNaa3U0M2FQWkZuT3pNRDM0NmFaWlpTU29yajRaWFh6RHc9PSIsImRldmljZVNpZ25hdHVyZSI6InkxSldtNFErNzJXYUE4bXlWQlNkWGFoU1R0S0E0cTlxWGNBWkdXdk1LR2hlWHZJU1NMamt6UlgvdUl2ajVVVTdOeVFaLzNObmRBcTJ6T1VUQ015VkRRPT0ifSwic2lnbmFsSWRlbnRpdGllcyI6W3siaWRlbnRpZmllciI6eyJuYW1lIjoiMjU3NjY0ODYzMDM6NzZAcy53aGF0c2FwcC5uZXQiLCJkZXZpY2VJZCI6MH0sImlkZW50aWZpZXJLZXkiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJCWkxaREkwTUVZb0QreUFjb21rK2tHZ3FsdGxBTGN6a1ZweFY1bitrUk54bCJ9fV0sInBsYXRmb3JtIjoiYW5kcm9pZCIsImxhc3RBY2NvdW50U3luY1RpbWVzdGFtcCI6MTcyMzUzODc2MywibXlBcHBTdGF0ZUtleUlkIjoiQUFBQUFJbGwifQ=="
 module.exports = {
   menu: process.env.MENU || "1",
   HANDLERS: process.env.PREFIX || "+.",
@@ -63,7 +63,7 @@ module.exports = {
   HEROKU: process.env.HEROKU_APP_NAME && process.env.HEROKU_API_KEY,
   aitts_Voice_Id: process.env.AITTS_ID || "37",
   ELEVENLAB_API_KEY: process.env.ELEVENLAB_API_KEY || "...d336",
-  WORKTYPE: process.env.WORKTYPE || process.env.MODE || "public",
+  WORKTYPE: process.env.WORKTYPE || process.env.MODE || "private",
   LANG: (process.env.THEME || "Kerm-md-v2").toUpperCase(),
 };
 global.rank = "updated";
