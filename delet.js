4d62f7027c6b6d7976415bedb96f629d70179f81
KermRayan. <kermd237@gmail.com>
2024-08-18 01:36:27 +0100

    Delete delet.js

--- delet.js
+++
@@ -1,5 +0,0 @@
-const jid = '1234@s.whatsapp.net' // can also be a group
-const response = await sock.sendMessage(jid, { text: 'hello!' }) // send a message
-// sends a message to delete the given message
-// this deletes the message for everyone
-await sock.sendMessage(jid, { efface: response.key })
