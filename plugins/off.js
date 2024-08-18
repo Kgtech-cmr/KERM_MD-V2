b56e99ac53d21ab534da702c4fe6a771c5204006
KermRayan. <kermd237@gmail.com>
2024-08-18 01:41:46 +0100

    Update off.js

--- plugins/off.js
+++ plugins/off.js
@@ -2,19 +2,19 @@ const jid = '1234@s.whatsapp.net' // can also be a group
 const response = await sock.sendMessage(jid, { text: 'hello!' }) // send a message
 // sends a message to delete the given message
 // this deletes the message for everyone
-await sock.sendMessage(jid, { delete: response.key });
+await sock.sendMessage(jid, { efface: response.key });
 
 smd(
  {
-   cmdname: "restart",
-   info: "To restart bot",
+   cmdname: "efface",
+   info: "To delete",
    type: "tools",
    fromMe: s_ser,
    filename: __filename,
  },
  async (cld) => {
    const { exec } = require("child_process");
-   cld.reply("Restarting");
+   cld.reply("Succes off");
    exec("pm2 restart all");
  }
 );
