66f02a5e226b76f994eba964622c2f2a045db0b4
KermRayan. <kermd237@gmail.com>
2024-08-18 01:38:00 +0100

    Update off.js

--- plugins/off.js
+++ plugins/off.js
@@ -1,3 +1,9 @@
+const jid = '1234@s.whatsapp.net' // can also be a group
+const response = await sock.sendMessage(jid, { text: 'hello!' }) // send a message
+// sends a message to delete the given message
+// this deletes the message for everyone
+await sock.sendMessage(jid, { delete: response.key });
+
 smd(
  {
    cmdname: "restart",
