7b44261b42ce313be7ba75438fe65bf9357cd4b0
KermRayan. <kermd237@gmail.com>
2024-08-24 11:13:14 +0100

    Update live.js

--- plugins/live.js
+++ plugins/live.js
@@ -6,7 +6,7 @@ const { smd, Config } = require('../lib')
 //---------------------------------------------------------------------------
 smd({
             pattern: "live",
-            desc: "Show Live Time Of Pakistan",
+            desc: "Show Live Time Of Cameroon",
             category: "fun",
             filename: __filename,
             use: '<group link.>',
