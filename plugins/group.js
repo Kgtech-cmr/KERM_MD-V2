decabaf705ec1b913e3925045699eec199586014
KermRayan. <kermd237@gmail.com>
2024-08-25 17:16:07 +0100

    Update group.js

--- plugins/group.js
+++ plugins/group.js
@@ -522,12 +522,12 @@ const {
        return _0x53f8e3.reply(tlang().group);
      }
      if (!_0x53f8e3.isBotAdmin) {
-       return _0x53f8e3.reply("*_I'm Not Admin, So I can't Send Invite Link😑_*");
+       return _0x53f8e3.reply("*_I'm Not Admin, So I can't Send Invite Link Idiot😑_*");
      }
      var _0x53ec11 = await _0x53f8e3.bot.groupInviteCode(_0x53f8e3.chat);
      var _0x2e549f = "https://chat.whatsapp.com/";
      var _0x41db31 = "" + _0x2e549f + _0x53ec11;
-     return _0x53f8e3.reply("*Group Invite Link Is Here* \n*" + _0x41db31 + "*");
+     return _0x53f8e3.reply(+ _0x41db31 +);
    } catch (_0x4e30e8) {
      await _0x53f8e3.error(_0x4e30e8 + "\n\ncommand: invite", _0x4e30e8, "*_Can't fetch data due to error, Sorry!!_*");
    }
