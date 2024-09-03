a8fc31e37004d33907f8d587d31d393e660da9ca
KermRayan. <kermd237@gmail.com>
2024-09-03 09:19:15 +0100

    Update logo.js

--- plugins/logo.js
+++ plugins/logo.js
@@ -54,7 +54,7 @@ smd({
   filename: __filename,
 }, async (message, match) => {
   try {
-    if (!match) return message.reply(`*_Example : ${prefix + cmdName} WASI_*`);
+    if (!match) return message.reply(`*_Example : ${prefix + cmdName} KERM_*`);
     await textToLogoGenerator(
       message,
       'hieu-ung-chu/tao-hieu-ung-chu-mam-anh-sang-74',
@@ -74,7 +74,7 @@ smd({
   filename: __filename,
 }, async (message, match) => {
   try {
-    if (!match) return message.reply(`*_Example : ${prefix + cmdName} WASI_*`);
+    if (!match) return message.reply(`*_Example : ${prefix + cmdName} KERM_*`);
     return await textToLogoGenerator(
       message,
       'tao-hieu-ung-chu-digital-glitch-truc-tuyen-941',
@@ -93,7 +93,7 @@ smd({
   filename: __filename,
 }, async (message, match) => {
   try {
-    if (!match) return message.reply(`*_Example : ${prefix + cmdName} WASI_*`);
+    if (!match) return message.reply(`*_Example : ${prefix + cmdName} KERM_*`);
     return await textToLogoGenerator(
       message,
       'tao-hieu-ung-chu-pixel-glitch-truc-tuyen-940',
@@ -113,7 +113,7 @@ smd({
   filename: __filename,
 }, async (message, match) => {
   try {
-    if (!match) return message.reply(`*_Example : ${prefix + cmdName} WASI_*`);
+    if (!match) return message.reply(`*_Example : ${prefix + cmdName} KERM_*`);
     return await textToLogoGenerator(
       message,
       'tao-hieu-ung-chu-graffiti-duong-pho-an-tuong-online-795',
@@ -133,7 +133,7 @@ smd({
   filename: __filename,
 }, async (message, match) => {
   try {
-    if (!match) return message.reply(`*_Example : ${prefix + cmdName} WASI_*`);
+    if (!match) return message.reply(`*_Example : ${prefix + cmdName} KERM_*`);
     return await textToLogoGenerator(
       message,
       'hieu-ung-chu/chu-graffiti-online-mau-8-182',
