0d61951fdfc5fddbaef499368e3698d5aaf3548e
KermRayan. <kermd237@gmail.com>
2024-09-18 07:31:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const bot = require(__dirname + '/lib/amd')
 const { VERSION } = require(__dirname + '/config')
 
 const start = async () => {
-    Debug.info(`Starting Asta ${VERSION}`)
+    Debug.info(`Starting Kerm ${VERSION}`)
   try {
     await bot.init()
     //bot.logger.info('⏳ Database syncing!')
